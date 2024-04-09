13b54a51543fda32615dac15243aa7e820e613bb
Patrik Reľovský <prelovsky@abboptical.com>
2024-04-09 12:07:34 +0200

    Add all properties to product type

--- src/types/product.d.ts
+++ src/types/product.d.ts
@@ -1,5 +1,17 @@
 export type Product = {
-  id: string;
+  form_action: string;
+  form_inputs: string;
+  formatted_price: string;
+  id: number;
+  image: string;
+  labels: string[];
   name: string;
   price: number;
+  product_url: string;
+  rating_summary: number;
+  reviews_count: number;
+  saleable: boolean;
+  sku: string;
+  small_image: string;
+  thumbnail: string;
 };
