2abc0357d5753694596d55aff86842650418566d
Patrik Reľovský <prelovsky@abboptical.com>
2024-04-09 15:07:53 +0200

    Implement product info component

--- src/components/ProductInfo/ProductInfo.tsx
+++ src/components/ProductInfo/ProductInfo.tsx
@@ -1,4 +1,6 @@
 import { Product } from "@/types";
+import Image from "next/image";
+import { Rating } from "../Rating";
 import styles from "./ProductInfo.module.scss";
 
 type Props = {
@@ -8,7 +10,23 @@ type Props = {
 export default function ProductInfo({ product }: Props) {
   return (
     <div className={styles.productInfo}>
-      <h3>{product.name}</h3>
+      <div className={styles.thumbnail}>
+        <Image
+          src={product.thumbnail}
+          alt="Product thumbnail"
+          width={1200}
+          height={1200}
+          sizes="(max-width: 768px) 140px, 168px"
+        />
+      </div>
+      <div className={styles.info}>
+        <h3 className={styles.name}>{product.name}</h3>
+        <Rating
+          ratingValue={product.rating_summary}
+          reviewsCount={product.reviews_count}
+        />
+        <p>{product.formatted_price}</p>
+      </div>
     </div>
   );
 }
