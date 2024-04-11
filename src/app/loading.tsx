a098548d9975748313053df2066a004a1a97ba9b
Patrik Reľovský <prelovsky@abboptical.com>
2024-04-11 11:29:33 +0200

    Add loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,18 @@
+import styles from "./loading.module.scss";
+
+export default function Loading() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.ldsRoller}>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
