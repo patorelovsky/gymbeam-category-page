0a8b0e96e173eb7809be3b30a3c67d67dfada195
Patrik Reľovský <prelovsky@abboptical.com>
2024-04-11 10:51:01 +0200

    Import missing types

--- src/types/getProductsResponse.d.ts
+++ src/types/getProductsResponse.d.ts
@@ -1,3 +1,7 @@
+import type { Filter } from "./filter";
+import type { Meta } from "./meta";
+import type { Product } from "./product";
+
 export type GetProductsResponse = {
   items: Product[];
   filters: Filter[];
