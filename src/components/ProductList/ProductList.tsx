3f50fb7477d4bc734a2f352cfb3faa6b6e9d54bd
Patrik Reľovský <prelovsky@abboptical.com>
2024-04-11 16:24:01 +0200

    Add no results message

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,4 +1,5 @@
 import type { Product } from "@/types";
+import { ImWondering2 } from "react-icons/im";
 import { ProductInfo } from "../ProductInfo";
 import styles from "./ProductList.module.scss";
 
@@ -7,11 +8,16 @@ type Props = {
 };
 
 export default function ProductList({ products }: Props) {
-  return (
+  return products.length > 0 ? (
     <div className={styles.productList}>
       {products.map((product) => (
         <ProductInfo key={product.id} product={product} />
       ))}
+    </div>
+  ) : (
+    <div className={styles.noResults}>
+      <ImWondering2 className={styles.icon} />
+      Nothing to see here. Try to adjust filters to see results.
     </div>
   );
 }
